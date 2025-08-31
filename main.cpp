72f33edb3f3f1d73241ab52be8de09c8046cf0a2
helyanny <helyanny@gmail.com>
2025-08-30 21:04:21 -0400

    Adding comments of my understanding

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <cmath>
 #include "tgaimage.h"
 
-constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
+constexpr TGAColor white   = {255, 255, 255, 255}; // BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
 constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
 void lineSegment(int ax, int bx, int ay, int by, TGAImage &framebuffer, TGAColor color) {
+	//Drawing line pixel by pixel (t is the slope)
 	for (int t = 0; t <= 1; t += 0.05) {
 	
 	}
@@ -15,10 +16,12 @@ void lineSegment(int ax, int bx, int ay, int by, TGAImage &framebuffer, TGAColor
 
 
 int main(int argc, char** argv) {
+	//Making frame
 	constexpr int width  = 64;
 	constexpr int height = 64;
 	TGAImage framebuffer(width, height, TGAImage::RGB);
 
+	//Drawing points
 	int ax =  7, ay =  3;
 	int bx = 12, by = 37;
 	int cx = 62, cy = 53;
@@ -27,6 +30,7 @@ int main(int argc, char** argv) {
 	framebuffer.set(bx, by, white);
 	framebuffer.set(cx, cy, white);
 
+	//Saving the frame
 	framebuffer.write_tga_file("framebuffer.tga");
 	return 0;
 }
